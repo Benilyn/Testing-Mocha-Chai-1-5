7e5bfa27a5079e00d7f3df5a2a7042b6bdc50d95
Benilyn <benilyn.gonzales@gmail.com>
2017-05-01 20:33:59 -0700

    added test codes for number divisible by 5 and 15

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -6,6 +6,8 @@ const fizzBuzzer = require('../fizzBuzzer');
 // unit tests for our `fizz-buzzer` function
 describe('fizzBuzzer', function() {
 
+  
+/*
   // test the normal case
   it('should divide a number', function() {
     // range of normal inputs, including
@@ -22,6 +24,31 @@ describe('fizzBuzzer', function() {
       answer.should.equal(input.expected);
     });
   });
+*/
+
+//return 'fizz-buzz' if num is divisible by 5
+  it('should return fizz-buzz is divisible by 15', function (){
+    const divisibleBy15 = [15, 30, 45, 60];
+    divisibleBy15.forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz-buzz')
+    }) //divisibleBy15
+  }); //return fizz-buzz
+
+  //return 'buzz' if num is divisible by 5
+  it('should return buzz is divisible by 5', function (){
+    const divisibleBy5 = [5, 10, 15, 20];
+    divisibleBy5.forEach(function(input) {
+      fizzBuzzer(input).should.equal('buzz')
+    }) //divisibleBy5
+  }); //return buzz
+
+  //return 'fizz' if num is divisible by 3
+  it('should return fizz is divisible by 3', function (){
+    const divisibleBy3 = [3, 6, 9, 12];
+    divisibleBy3.forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz')
+    }) //divisibleBy3
+  }); //return fizz
 
   //return number if not divisible by 15, 5 or 3
   it('should return num if not divisible by 15, 5 or 3', function(){
